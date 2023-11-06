ab88677e3c7055d9ad6d22723e45c76f3803352d
supriyaedamalapati <supriyaedamalapati72@gmail.com>
2023-11-06 02:28:00 -0500

    login

--- FrontEnd/ecommerce/src/login/registration/loginpage/loginpage.component.ts
+++ FrontEnd/ecommerce/src/login/registration/loginpage/loginpage.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loginpage',
   templateUrl: './loginpage.component.html',
   styleUrls: ['./loginpage.component.scss']
 })
-export class LoginpageComponent {
+export class LoginpageComponent implements OnInit {
+  hidePassword: boolean = true;
 
+  loginFormGroup!: FormGroup;
+  forgotPasswordGroup!: FormGroup;
+
+
+  constructor(
+    private _router: Router,
+    public dialog: MatDialog,
+    private _snackBar: MatSnackBar,
+    private fb: FormBuilder // Inject FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.loginFormGroup = this.fb.group({
+      UsernameCtrl: ['', [Validators.required, Validators.email]],
+      PasswordCtrl: ['', [Validators.required]],
+      remember: [false],
+    });
+
+    this.forgotPasswordGroup = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+
+    this.loginFormGroup.controls['UsernameCtrl'].setValue('');
+    this.loginFormGroup.controls['PasswordCtrl'].setValue('');
+
+  }
+
+  login() {
+    // Here you can add your login logic using this.loginFormGroup.value
+    const username = this.loginFormGroup.value.UsernameCtrl;
+    const password = this.loginFormGroup.value.PasswordCtrl;
+
+    if (username === 'your_username' && password === 'your_password') {
+      alert('Login successful!');
+    } else {
+      alert('Invalid username or password. Please try again.');
+    }
+  }
+
+  forgot_password() {
+    // Handle forgot password logic here
+  }
 }
--- FrontEnd/ecommerce/src/login/registration/registration.module.ts
+++ FrontEnd/ecommerce/src/login/registration/registration.module.ts
@@ -2,6 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginpageComponent } from './loginpage/loginpage.component';
 import { RegisterpageComponent } from './registerpage/registerpage.component';
+import {Component, Inject} from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
+import {NgIf} from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+
 
 
 
@@ -11,7 +20,18 @@ import { RegisterpageComponent } from './registerpage/registerpage.component';
     RegisterpageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    NgIf,
+    MatDialogModule,
+    MatSnackBarModule
+  ],
+  exports:[
+    LoginpageComponent,
+    RegisterpageComponent
   ]
 })
 export class RegistrationModule { }
